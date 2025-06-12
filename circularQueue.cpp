d534106152f5e6dede80fa4f0654b30e73ba01a9
Krisna <your-email@example.com>
2025-06-12 17:40:22 +0700

    else while

--- circularQueue.cpp
+++ circularQueue.cpp
@@ -80,6 +80,18 @@ public:
                 cout << queue_array[FRONT_position] << " ";
                 FRONT_position++;
             }
-        }  
+        }
+        else {
+            while (FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            FRONT_position = 0;
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+        }
+        cout << endl;
     }
 };
