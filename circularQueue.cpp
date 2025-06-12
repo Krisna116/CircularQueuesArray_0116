ebf4d98a232847cb66c02ee76834bacac2b1d9e8
Krisna <your-email@example.com>
2025-06-12 17:30:50 +0700

    2. cek apakah antrian kosong

--- circularQueue.cpp
+++ circularQueue.cpp
@@ -24,6 +24,11 @@ public:
             cout << "\nQueue overflow\n";
             return;
         }
-       
+
+        // 2. cek apakah antrian kosong 
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        } 
     }
-};
+}
